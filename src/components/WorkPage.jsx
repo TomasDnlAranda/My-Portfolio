4fbbbe67e25e67310b447c7f0cc425a2bf9a31f1
Tomás Aranda <tomasdeveloperfrontend@gmail.com>
2022-04-24 02:18:00 -0300

    add-work

--- src/components/WorkPage.jsx
+++ src/components/WorkPage.jsx
@@ -1,7 +1,92 @@
 import React from 'react';
+import { ThemeProvider } from 'styled-components';
+import styled from 'styled-components';
+import { darkTheme } from './Themes';
+import LogoComponent from '../subComponents/LogoComponent';
+import PowerButton from '../subComponents/PowerButton';
+import SocialIcons from '../subComponents/SocialIcons';
+import { Work } from '../data/WorkData';
+import CardWork from '../subComponents/CardWork';
+import { useRef, useEffect } from 'react';
+import { darkTheme as A } from './Themes';
 
+const Box = styled.div`
+	background-color: ${(props) => props.theme.body};
+	height: 400vh;
+	align-items: center;
+	overflow: hidden;
+`;
+
+const Main = styled.ul`
+	position: fixed;
+	top: 12rem;
+	left: calc(10rem + 15vw);
+	height: 40vh;
+	display: flex;
+
+	color: red;
+`;
+
+const Rotate = styled.span`
+	display: block;
+	position: fixed;
+	right: 1rem;
+
+	bottom: 1rem;
+	width: 80px;
+	height: 80px;
+	z-index: 1;
+`;
 const WorkPage = () => {
-	return <div>WorkPage</div>;
+	const ref = useRef(null);
+	const yinyang = useRef(null);
+
+	useEffect(() => {
+		let element = ref.current;
+
+		const rotate = () => {
+			element.style.transform = `translateX(${-window.pageYOffset}px)`;
+
+			yinyang.current.style.transform = `rotate(` + -window.pageYOffset + 'deg)';
+		};
+		window.addEventListener('scroll', rotate);
+		return () => window.removeEventListener('scroll', rotate);
+	}, []);
+
+	return (
+		<ThemeProvider theme={darkTheme}>
+			<Box>
+				<LogoComponent theme="dark" />
+				<SocialIcons theme="dark" />
+				<PowerButton />
+				<Main ref={ref}>
+					{Work.map((item) => (
+						<CardWork key={item.id} item={item} />
+					))}
+				</Main>
+				<Rotate ref={yinyang}>
+					<svg
+						width={80}
+						height={80}
+						color="#fff"
+						aria-hidden="true"
+						focusable="false"
+						data-prefix="fas"
+						data-icon="yin-yang"
+						class="svg-inline--fa fa-yin-yang fa-w-16"
+						role="img"
+						xmlns="http://www.w3.org/2000/svg"
+						viewBox="0 0 496 512"
+					>
+						<path
+							fill="currentColor"
+							d="M248 8C111.03 8 0 119.03 0 256s111.03 248 248 248 248-111.03 248-248S384.97 8 248 8zm0 376c-17.67 0-32-14.33-32-32s14.33-32 32-32 32 14.33 32 32-14.33 32-32 32zm0-128c-53.02 0-96 42.98-96 96s42.98 96 96 96c-106.04 0-192-85.96-192-192S141.96 64 248 64c53.02 0 96 42.98 96 96s-42.98 96-96 96zm0-128c-17.67 0-32 14.33-32 32s14.33 32 32 32 32-14.33 32-32-14.33-32-32-32z"
+						></path>
+					</svg>
+				</Rotate>
+			</Box>
+		</ThemeProvider>
+	);
 };
 
 export default WorkPage;
